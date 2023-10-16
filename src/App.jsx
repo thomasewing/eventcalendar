b82c8cabf751d8e29ba8a5a6b6a31cc4a60c24a2
thomasewing <31376385+thomasewing@users.noreply.github.com>
2023-10-16 14:11:49 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { useState } from 'react';
-import axios from 'axios';
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import EventTrackerHome from './components/EventTrackerHome';
-import EventTracker from './components/EventTracker';
-import EventList from './components/EventList';
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [holiday, setHoliday] = useState('')
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<EventTrackerHome setHoliday={setHoliday} />} />
-          <Route path="/eventtracker/:holiday" element={<EventTracker holiday={holiday} setHoliday={setHoliday} />} />
-          <Route path="/eventtracker/:holiday/eventlist" element={<EventList holiday={holiday} setHoliday={setHoliday} />} />
-        </Routes>
-      </BrowserRouter>
-    
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
 
 export default App
-
