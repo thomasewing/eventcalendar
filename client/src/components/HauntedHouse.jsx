5f041f703f80203beb4e6103c14e6f551c3bee75
Claudia Ledezma <110560041+Claudialedez@users.noreply.github.com>
2023-10-18 14:05:04 -0500

    Create HauntedHouse.jsx

---
+++ client/src/components/HauntedHouse.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+
+function About(){
+    return(
+        <div className="about">
+            <div 
+            className="aboutTop"
+            style={{backgroundColor:#AA0000}}
+            >
+                <h1>Horrifying Spooky Haunted House</h1>
+                <p>October 31st, 2023</p>
+                <p>666 Spooky Ave</p>
+                <p>Scare Level</p>
+        
+            </div>
+        </div>
+
+
+    )
+}
+function sayEdit() {
+    alert('EDIT!')
+  }
+  <button onClick={sayEdit}>Edit</button>;
+
+  function sayDelete() {
+    alert('DELETE!')
+  }
+  <button onClick={sayDelete}>Delete</button>;
